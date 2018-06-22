eb13514d6b0da0c67424b14a31601bcaf148318f
andrei cirlan <andreynho2006@gmail.com>
2018-06-22 17:58:55 +0300

    add eventListener for users on click

--- colorGame.js
+++ colorGame.js
@@ -1,19 +1,35 @@
 var colors = [
-    "rgb(255, 0,0)",
-    "rgb(255, 255,0)",
-    "rgb(0, 255,0)",
-    "rgb(0, 255,255)",
-    "rgb(0, 0,255)",
-    "rgb(255, 0,255)",
-];
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+]
 
 var squares = document.querySelectorAll(".square");
 
 var pickedColor = colors[3];
+console.log(pickedColor);
 var colorDisplay = document.getElementById("colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
-for( var i =0; i < squares.length; i++) {
+for( var i = 0; i < squares.length; i++) {
+    // add initials color to squares
     squares[i].style.background = colors[i];
+
+    //add click listeners to squares
+    squares[i].addEventListener("click", function() {
+        //grab color of clicked square
+        var clickedColor = this.style.background;
+        console.log(clickedColor);
+        //compare color to picked color
+        if(clickedColor === pickedColor) {
+            alert("Corect");
+        } else {
+            alert("Wrong");
+        }
+    });
 }
+
