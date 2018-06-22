96b05866a01e573f37155ef7f911b4af7443d091
andrei cirlan <andreynho2006@gmail.com>
2018-06-22 17:33:54 +0300

    make the index file

--- colorGame.js
+++ colorGame.js
@@ -1,14 +1,14 @@
 var colors = [
-    "rgb(255, 0,0"),
-    "rgb(255, 255,0"),
-    "rgb(0, 255,0"),
-    "rgb(0, 255,255"),
-    "rgb(0, 0,255"),
-    "rgb(255, 0,255"),
+    "rgb(255, 0,0)",
+    "rgb(255, 255,0)",
+    "rgb(0, 255,0)",
+    "rgb(0, 255,255)",
+    "rgb(0, 0,255)",
+    "rgb(255, 0,255)",
 ];
 
 var squares = document.querySelectorAll(".square");
 
-fao( var i =0; i < square.length; i++) {
-    squares[i].style.background = color[i];
+for( var i =0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
 }
